d4572d1e40ab5c39b4490ca5b69fb672df055775
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-11-03 17:48:58 +0200

    Add AddressBook class

---
+++ models/address_book.py
@@ -0,0 +1,42 @@
+from collections import UserDict
+from datetime import datetime, timedelta
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def get_upcoming_birthdays(self):
+        today = datetime.today().date()
+        upcoming_birthdays = []
+        
+        for record in self.data.values():
+            if record.birthday:
+                birthday_date = record.birthday.value
+                birthday_this_year = birthday_date.replace(year=today.year)
+                
+                if birthday_this_year < today:
+                    birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+                
+                days_until_birthday = (birthday_this_year - today).days
+                
+                if 0 <= days_until_birthday <= 7:
+                    congratulation_date = birthday_this_year
+                    
+                    if congratulation_date.weekday() == 5:
+                        congratulation_date += timedelta(days=2)
+                    elif congratulation_date.weekday() == 6:
+                        congratulation_date += timedelta(days=1)
+                    
+                    upcoming_birthdays.append({
+                        "name": record.name.value,
+                        "congratulation_date": congratulation_date.strftime("%d.%m.%Y")
+                    })
+        
+        return upcoming_birthdays
